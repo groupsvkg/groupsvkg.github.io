468b7672966c14ec019bf4a0406f1755a4570576
Vishal Kumar Gupta <groupsvkg@gmail.com>
2021-07-12 11:11:27 +0100

    implemented arrow for initial state

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,14 +8,14 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "version.json": "541096d10ffabdf87c7d95bd2701c9d7",
-"main.dart.js": "5a066308c80068c96b2b6fb744fa089a",
+"main.dart.js": "80d1a5040019dfd9ead9446f6094cb3a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "813cf69c27cef9cf1a375638788670e9",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "05aa937ec9f4f78b0636834449e5a521",
-"/": "05aa937ec9f4f78b0636834449e5a521"
+"index.html": "ff85c7e9e7490aab27ca5ce35f82d4c2",
+"/": "ff85c7e9e7490aab27ca5ce35f82d4c2"
 };
 
 // The application shell files that are downloaded before a service worker can
