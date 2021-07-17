e3256f7805e42b7b53e5d241c5ab25504a5c757b
Vishal Kumar Gupta <groupsvkg@gmail.com>
2021-07-17 17:17:37 +0100

    implemented label on transition arrows

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,14 +8,14 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "version.json": "541096d10ffabdf87c7d95bd2701c9d7",
-"main.dart.js": "bffe9e9c509832b40be82a7584f752f5",
+"main.dart.js": "5a39edd68caa2c44a52b55b472feb1aa",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "813cf69c27cef9cf1a375638788670e9",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "668c4c140476408a6d69e12100fc7f9b",
-"/": "668c4c140476408a6d69e12100fc7f9b"
+"index.html": "cbc8d1b603cd53ff900850eb082d3e4b",
+"/": "cbc8d1b603cd53ff900850eb082d3e4b"
 };
 
 // The application shell files that are downloaded before a service worker can
