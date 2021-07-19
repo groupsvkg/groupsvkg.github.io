7cf902ecb90ea39a2a8cac8176bb90514f351f32
Vishal Kumar Gupta <groupsvkg@gmail.com>
2021-07-19 10:32:46 +0100

    minor fix for bend right attribute

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,14 +8,14 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "version.json": "541096d10ffabdf87c7d95bd2701c9d7",
-"main.dart.js": "3a039653769e7f80d05d8243f1e10150",
+"main.dart.js": "85184e65adc747dc194b3bacf8593e6e",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "813cf69c27cef9cf1a375638788670e9",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "76a63113832969bd5b4afcee94aace0e",
-"/": "76a63113832969bd5b4afcee94aace0e"
+"index.html": "5beabe6d8ffd4ad9e014c5a1e0b84d73",
+"/": "5beabe6d8ffd4ad9e014c5a1e0b84d73"
 };
 
 // The application shell files that are downloaded before a service worker can
