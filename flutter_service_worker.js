5a9b275c85dee7d96ab5e0269c29ffd588a0febf
Vishal Kumar Gupta <groupsvkg@gmail.com>
2021-07-26 22:13:34 +0100

    minor fix for from, to, and max parameter for play command

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,14 +8,14 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "version.json": "541096d10ffabdf87c7d95bd2701c9d7",
-"main.dart.js": "aa351c69742845710ed67d0f535dfc04",
+"main.dart.js": "88c6252a4bb6c81fa803edb446e96885",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "813cf69c27cef9cf1a375638788670e9",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "b5c9803828d73128346d7d8970dfa2aa",
-"/": "b5c9803828d73128346d7d8970dfa2aa"
+"index.html": "4864d9b0f58f5d7653d78239f3ae3899",
+"/": "4864d9b0f58f5d7653d78239f3ae3899"
 };
 
 // The application shell files that are downloaded before a service worker can
